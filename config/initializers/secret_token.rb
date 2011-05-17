f5d4de896aeccfe298a0b5fb573c2e2af11e2734
4pcbr <o.s.sidorov@gmail.com>
2011-05-18 00:15:30 +0400

    создал проект tips

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tips::Application.config.secret_token = 'c807c167f0d0a5abb10921769a49bd14edf63cbccfc220081ee2a915007b9c801b65d5f81fc6e36801d676cc9c31616ec9ecad9feb127c1c883b8437a5148c8b'
